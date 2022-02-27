16dd8889ff75bc45f60fd49c7c41acf3500ced57
mio kataoka <mio.kanna@gmail.com>
2022-02-26 20:44:36 -0800

    commit tree 89d87bad68378675fa15fc9a5bb0ef036c8e9787

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: `mio`,
+    streetAddress: `6465 San Pablo Ave`,
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee.streetAddress = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.streetAddress = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const anotherEmployee = {...employee};
+    delete anotherEmployee.name;
+    return anotherEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee;
+}
